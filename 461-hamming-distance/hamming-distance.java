8e1555e64e6a29a8843630f200415e58c4a698a1
hasim-S <hasimsmohamed@gmail.com>
2026-06-22 14:37:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (55.36%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int a = 0;
+
+        while (x != 0 || y != 0) {
+            if ((x & 1) != (y & 1)) {
+                a++;
+            }
+
+            x = x >> 1;
+            y = y >> 1;
+        }
+
+        return a;
+    }
+}
