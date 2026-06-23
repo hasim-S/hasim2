624cb232ab89a748c06c81d72c25efecb99d32d3
hasim-S <hasimsmohamed@gmail.com>
2026-06-23 23:52:16 +0530

    Time: 1684 ms (5.16%) | Memory: 47 MB (45.88%) - LeetSync

---
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class Solution {
+    public int[] findEvenNumbers(int[] digits) {
+       // int h=1;
+                int n=digits.length;
+        //for(int i=1;i<=n;i++)
+        //h*=i;
+        int a[]=new  int[450];
+
+        int x=0;
+        for(int i=0;i<n;i++)
+        {
+            if(digits[i]!=0)
+            {
+            for(int j=0;j<n;j++)
+            {
+                if(j!=i)
+                {
+                for(int k=0;k<n;k++)
+                {
+                     if((k!=i)&&(k!=j))
+                     {
+                        if(digits[k]%2==0)
+                        {
+                            int flag=0;
+                            int t=(digits[i]*100)+(digits[j]*10)+digits[k];
+                            for(int l=0;l<x;l++)
+                            {
+                                if(t==a[l])
+                                {
+                                flag=1;
+                                break;
+                                }
+                            }
+                            if(flag==0)
+                            a[x++]=t;
+                        }
+                     }
+                }
+            }
+            }
+        }
+        }
+        int b[]=new int[x];
+        for(int i=0;i<x;i++)
+        {
+            b[i]=a[i];
+        }
+        Arrays.sort(b);
+        return b;
+    }
+}
