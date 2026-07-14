c74d37c858d88643248abe50f6a95b0239184a34
hasim-S <hasimsmohamed@gmail.com>
2026-07-14 15:13:50 +0530

    Time: 3 ms (35.74%) | Memory: 43.1 MB (17.66%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        HashSet<Character> set = new HashSet<>();
+
+        for (char ch : sentence.toCharArray()) {
+            set.add(ch);
+        }
+
+        return set.size() == 26;
+    }
+}
